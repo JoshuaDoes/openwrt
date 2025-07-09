580d0a4e1e35cefcab1bc045e68dc8fd674f8623
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2025-07-09 08:13:10 -0400

    tensor: Checkout specific git revisions

--- target/linux/tensor/image/gs101.mk
+++ target/linux/tensor/image/gs101.mk
@@ -6,6 +6,7 @@ define Device/google_oriole
   $(call Device/tensor)
   DEVICE_MODEL := Pixel 6
   DEVICE_PACKAGES := tensor-kernel-prebuilt-raviole
+  TENSOR_CHECKOUT := ff96f402ce6f3be87cb40a16f33ac62d69cdb16a
   TENSOR_PRODUCT := raviole
   TENSOR_CMDLINE := cmdline=fips140.load_sequential=1 exynos_mfc.load_sequential=1 exynos_drm.load_sequential=1 g2d.load_sequential=1 disable_dma32=on earlycon=exynos4210,0x10A00000 console=ttySAC0,115200 androidboot.console=ttySAC0 printk.devkmsg=on cma_sysfs.experimental=Y cgroup_disable=memory cgroup.memory=nokmem rcupdate.rcu_expedited=1 rcu_nocbs=all rcutree.enable_rcu_lazy swiotlb=noforce disable_dma32=on sysctl.kernel.sched_pelt_multiplier=4 at24.write_timeout=100 log_buf_len=1024K
   TENSOR_BASE := 0x80000000
@@ -21,6 +22,7 @@ define Device/google_raven
   $(call Device/tensor)
   DEVICE_MODEL := Pixel 6 Pro
   DEVICE_PACKAGES := tensor-kernel-prebuilt-raviole
+  TENSOR_CHECKOUT := ff96f402ce6f3be87cb40a16f33ac62d69cdb16a
   TENSOR_PRODUCT := raviole
   TENSOR_CMDLINE := cmdline=fips140.load_sequential=1 exynos_mfc.load_sequential=1 exynos_drm.load_sequential=1 g2d.load_sequential=1 disable_dma32=on earlycon=exynos4210,0x10A00000 console=ttySAC0,115200 androidboot.console=ttySAC0 printk.devkmsg=on cma_sysfs.experimental=Y cgroup_disable=memory cgroup.memory=nokmem rcupdate.rcu_expedited=1 rcu_nocbs=all rcutree.enable_rcu_lazy swiotlb=noforce disable_dma32=on sysctl.kernel.sched_pelt_multiplier=4 at24.write_timeout=100 log_buf_len=1024K
   TENSOR_BASE := 0x80000000
@@ -36,6 +38,7 @@ define Device/google_bluejay
   $(call Device/tensor)
   DEVICE_MODEL := Pixel 6a
   DEVICE_PACKAGES := tensor-kernel-prebuilt-bluejay
+  TENSOR_CHECKOUT := f8676058a939195f75e748184aeea572a6a522bf
   TENSOR_PRODUCT := bluejay
   TENSOR_CMDLINE := cmdline=fips140.load_sequential=1 exynos_mfc.load_sequential=1 exynos_drm.load_sequential=1 g2d.load_sequential=1 disable_dma32=on earlycon=exynos4210,0x10A00000 console=ttySAC0,115200 androidboot.console=ttySAC0 printk.devkmsg=on cma_sysfs.experimental=Y cgroup_disable=memory cgroup.memory=nokmem rcupdate.rcu_expedited=1 rcu_nocbs=all rcutree.enable_rcu_lazy swiotlb=noforce disable_dma32=on sysctl.kernel.sched_pelt_multiplier=4 at24.write_timeout=100 log_buf_len=1024K
   TENSOR_BASE := 0x80000000
